0bf0e428cfd4f263cd5ae6f8df3030e6957d3caf
Bharat Soni <bharat.soni@botreetechnologies.com>
2016-07-21 19:26:40 +0530

    Added the formatting of the code.

--- js/jquery.timestamp-rails.js
+++ js/jquery.timestamp-rails.js
@@ -1,38 +1,33 @@
-(function($) {
+( function($) {
+	$.fn.timestampRails = function(options) {
 
-    $.fn.timestampRails = function( options ) {
+		// Establish our default settings
+		var settings = $.extend({
+			format : 'dd-mm-yy',
+			separator : null
+          }, options);
 
-        // Establish our default settings
-        var settings = $.extend({
-            format         : 'dd-mm-yy',
-            separator      : null
-        }, options);
+		this.each(function() {
+			var date = new Date($(this).text());
+			var formatted_date = date.getDate() + '-' + (date.getMonth() + 1) + '-' + date.getFullYear();
 
-        this.each( function() {
-            var date = new Date($(this).text());
-		        var formatted_date = date.getDate() + '-' + (date.getMonth() + 1) + '-' +  date.getFullYear();
-            
-            if (settings.format){
-		          if ( settings.format == "mm-dd-yy" ) {
-								formatted_date = (date.getMonth() + 1) + '-' + date.getDate() + '-' +  date.getFullYear();
-		            }
-							else if ( settings.format == "yy-mm-dd" ){
-								formatted_date = date.getFullYear() + '-' + date.getDate() + '-' + (date.getMonth() + 1);
-		            }
-							else{
-								formatted_date = date.getDate() + '-' + (date.getMonth() + 1) + '-' +  date.getFullYear();
-							}
-						}
+			if (settings.format) {
+				if (settings.format == "mm-dd-yy") {
+					formatted_date = (date.getMonth() + 1) + '-' + date.getDate() + '-' + date.getFullYear();
+				} else if (settings.format == "yy-mm-dd") {
+					formatted_date = date.getFullYear() + '-' + date.getDate() + '-' + (date.getMonth() + 1);
+				} else {
+					formatted_date = date.getDate() + '-' + (date.getMonth() + 1) + '-' + date.getFullYear();
+				}
+			}
 
-						if ( settings.separator ) {
-								formatted_date = formatted_date.replace(/(-)+/g, settings.separator);
-						}            
-            
+			if (settings.separator) {
+				formatted_date = formatted_date.replace(/(-)+/g, settings.separator);
+			}
 
-		        // Finally replace the actual DOM with the value.
-						$(this).text(formatted_date);
-        });
+			// Finally replace the actual DOM with the value.
+			$(this).text(formatted_date);
+		});
 
-    }
-
-}(jQuery));
+	};
+}(jQuery)); 
